a3ac9415e12d77a9d851e6716cc1a8bf62f5f844
Marcos J. Alvarez <garusis@gmail.com>
2016-12-04 02:21:08 -0500

    Add basic mocha and chai support for unit tests

---
+++ test/helper.js
@@ -0,0 +1,12 @@
+/**
+ * Created by garusis on 04/12/16.
+ */
+const chai = require('chai');
+const _ = require('lodash');
+const app = require('../dist/server/server');
+
+global.should = chai.should();
+global.expect = chai.expect;
+global.assert = chai.assert;
+global._ = _;
+global.app = app;
