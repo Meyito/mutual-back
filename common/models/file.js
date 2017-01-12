18122d1b4da321d1ccd12d5587742482788ce14e
Marcos J. Alvarez <garusis@gmail.com>
2017-01-11 21:10:47 -0500

    Add gitkeep file filter

--- common/models/file.js
+++ common/models/file.js
@@ -1,5 +1,11 @@
 'use strict';
+module.exports = function (File) {
 
-module.exports = function(File) {
+  const _ = require('lodash');
+
+  File.afterRemote('getFiles', function (context, remoteMethodOutput, next) {
+    _.remove(context.result, (file) => file.name === '.gitkeep');
+    next();
+  });
 
 };
