fe030bcfe81422b30a7b3194185eca3289838e29
Marcos J. Alvarez <garusis@gmail.com>
2017-01-20 20:47:02 -0500

    Add indexes

--- common/mixins/timestamp.js
+++ common/mixins/timestamp.js
@@ -3,15 +3,15 @@
 /**
  * Created by garusis on 08/12/16.
  */
-module.exports = function(Model, options) {
+module.exports = function (Model, options) {
   // Model is the model class
   // options is an object containing the config properties from model definition
-  Model.defineProperty('created', {type: Date, default: '$now'});
-  Model.defineProperty('modified', {type: Date, default: '$now'});
+  Model.defineProperty('created', {type: Date, default: '$now', index: true});
+  Model.defineProperty('modified', {type: Date, default: '$now', index: true});
 
   Model.observe('before save', function event(ctx, next) { //Observe any insert/update event on Model
     let data = ctx.instance || ctx.data;
-    if(ctx.isNewInstance){
+    if (ctx.isNewInstance) {
       data.created = Date.now();
     }
     data.modified = Date.now();
