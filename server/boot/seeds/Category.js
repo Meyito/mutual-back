8144ff73104c04ec87503a238ea81bdae27733d6
Marcos J. Alvarez <garusis@gmail.com>
2017-01-26 10:41:13 -0500

    Add categories seed

--- server/boot/seeds/Category.js
+++ server/boot/seeds/Category.js
@@ -2,16 +2,10 @@
  * Created by garusis on 08/12/16.
  */
 module.exports = [
-  {
-    name: 'Confianza',
-    slug: 'confianza'
-  },
-  {
-    name: 'Afecto',
-    slug: 'afecto'
-  },
-  {
-    name: 'Entretenimiento',
-    slug: 'entretenimiento'
-  }
+  {name: 'Padres Exploradores', slug: 'Padres Exploradores'},
+  {name: 'Padres Atletas', slug: 'Padres Atletas'},
+  {name: 'Padres Altruista', slug: 'Padres Altruista'},
+  {name: 'Padres Misticos', slug: 'Padres Misticos'},
+  {name: 'Padres Ecologicos', slug: 'Padres Ecologicos'},
+  {name: 'Padres Psiquicos', slug: 'Padres Psiquicos'}
 ];
