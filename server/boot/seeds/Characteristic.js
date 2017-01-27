c42a02f10e510cef8205da25070f362a6e80f23d
Marcos J. Alvarez <garusis@gmail.com>
2017-01-27 15:45:05 -0500

    Modify characteristic seeder

--- server/boot/seeds/Characteristic.js
+++ server/boot/seeds/Characteristic.js
@@ -3,24 +3,138 @@
  */
 module.exports = [
   {
-    name: 'autoconfianza',
-    label: 'Autoconfianza',
-    weight: 5,
-    minValueAlert: -10,
-    alertMessage: 'Ojo: Bajo nivel en Autoconfianza :('
+    name: "Saltar, brincar y marchar",
+    label: "Saltar, brincar y marchar",
+    alertMessage: " Enséñale a tu hijo los beneficios que tiene la actividad física."
   },
   {
-    name: 'conffamiliar',
-    label: 'Confianza Familiar',
-    weight: 3,
-    minValueAlert: -10,
-    alertMessage: 'Ojo: Bajo nivel en Confianza en la familia :('
+    name: "  Generoso",
+    label: "  Generoso",
+    alertMessage: " Dale a tu hijo ejemplo de solidaridad"
   },
   {
-    name: 'comunicacion',
-    label: 'Comunicación',
-    weight: 4,
-    minValueAlert: -10,
-    alertMessage: 'Ojo: Bajo nivel en Comunicacion :('
+    name: "Afectuoso",
+    label: "Afectuoso",
+    alertMessage: " Manifiéstate a tu hijo con frecuencia el amor que sientes por el."
+  },
+  {
+    name: "  Curioso",
+    label: "  Curioso",
+    alertMessage: " Responde las preguntas que genere tu hijo. Si no cuentas con una respuesta invitalo a que investiguen juntos."
+  },
+  {
+    name: "  Ansioso",
+    label: "  Ansioso",
+    alertMessage: " Manifiestale a tu hijo que siempre estarás cerca de él o ella por si necesita ayuda."
+  },
+  {
+    name: "  Egocentrismo",
+    label: "  Egocentrismo",
+    alertMessage: " Explícale a tu hijo los beneficios de compartir y pensar en las demás personas, no sólo en sí mismo."
+  },
+  {
+    name: "  Es muy activo. Suele ser ruidoso, inquieto y vivaz",
+    label: "  Es muy activo. Suele ser ruidoso, inquieto y vivaz",
+    alertMessage: " Si eres un padre o madre sedentaria, procura no forzar a tu hijo a hacer actividad física, la mejor forma de enseñar es el ejemplo. Si tú realizas ejercicios ellos lo harán."
+  },
+  {
+    name: "  Egoísta",
+    label: "  Egoísta",
+    alertMessage: " Explícale los sentimientos que puede tener otra persona si no compartes, no con el ánimo de hacerlo sentir culpable sino que genere empatía."
+  },
+  {
+    name: "  Anhela ser aprobado socialmente",
+    label: "  Anhela ser aprobado socialmente",
+    alertMessage: " Recuérdale a tu hijo los logros anteriores que haya tenido, con el fin de motivarlo."
+  },
+  {
+    name: "  Participa en juegos organizados que requieren habilidades físicas",
+    label: "  Participa en juegos organizados que requieren habilidades físicas",
+    alertMessage: " Enséñale que la actividad física debes ser tomada como un juego y no como una obligación o una competencia entre padres y niños."
+  },
+  {
+    name: "  Automotivación",
+    label: "  Automotivación",
+    alertMessage: " Convierte la actividad física en juego, la monotonía puede desmotivar a los niños, usa actividades y movimientos variados."
+  },
+  {
+    name: "  Responsable: Enséñale que todas las cosas tienen su tiempo",
+    label: "  Responsable: Enséñale que todas las cosas tienen su tiempo",
+    alertMessage: " estudiar, ver televisión, jugar con sus amigos, etc."
+  },
+  {
+    name: "  Se preocupa acerca de no hacer bien las cosas",
+    label: "  Se preocupa acerca de no hacer bien las cosas",
+    alertMessage: " No le hagas las cosas por temor a que se equivoque, deja que cometa errores durante el desarrollo de sus actividades diarias y oriéntalo permitiéndole que él mismo razone sobre la situación, esto le permitirá comprender que en algunas ocasiones es normal cometer errores."
+  },
+  {
+    name: "  Perfeccionista",
+    label: "  Perfeccionista",
+    alertMessage: " Reconoce los esfuerzos de tu hijo al realizar las actividades o tareas de su dia a dia."
+  },
+  {
+    name: "  Es cohibido y cauto",
+    label: "  Es cohibido y cauto",
+    alertMessage: " Intente hacerle entender que en algunos momentos es bueno tomar pequeños riesgos, aunque pueda fracasar siempre un riesgo es la oportunidad para tener éxito."
+  },
+  {
+    name: "  Conversaciones",
+    label: "  Conversaciones",
+    alertMessage: " Comparte experiencias con tus hijos, los niños y niñas a partir de los 5 años muestran interés por los relatos, para las edades de 5 a 7 se recomienda que sean historias cortas, debido a que el desarrollo y sostenimiento de su atención es muy corto, en edades de 7 a 11 se pueden dar más detalles de las historias o anécdotas y finalmente edades de 11 a 12 puede mantener conversaciones y llegar a compartir sus opiniones o sentimientos."
+  },
+  {
+    name: "  Reconoce las consecuencias de su forma de actuar",
+    label: "  Reconoce las consecuencias de su forma de actuar",
+    alertMessage: " Enséñale a respetar los entornos donde se desarrolle, ej. No botar basuras a las calles, no maltratar los animales, no dañar objetos del parque, cuidar las plantas. Si lo haces desde el seno de tu hogar estas conductas serán tomadas por el niño como habituales y las aplicará en todo lugar donde se encuentre."
+  },
+  {
+    name: "  Conciencia de lo bueno y lo malo",
+    label: "  Conciencia de lo bueno y lo malo",
+    alertMessage: " Sé un ejemplo para tu hijo o hija de tu visión y filosofía espiritual. Educalo no solo con la palabra sino con el ejemplo."
+  },
+  {
+    name: "  Introspección",
+    label: "  Introspección",
+    alertMessage: " Préstale atención a tu hijo y mantén siempre contacto visual directo a los ojos para que observe que le estás prestando atención."
+  },
+  {
+    name: "  Interactúa poco con personas del otro sexo",
+    label: "  Interactúa poco con personas del otro sexo",
+    alertMessage: " De ser posible vincula a más niños o niñas a la actividades (amiguitos, vecinos, primos, etc.)  y deja claro que son juegos y no competencias, esto permitirá que se sientan cómodos y se puedan realizar las actividades."
+  },
+  {
+    name: "  Pasa mucho más tiempo con sus amigos",
+    label: "  Pasa mucho más tiempo con sus amigos",
+    alertMessage: " De ser posible vincula a más niños o niñas a la actividades (amiguitos, vecinos, primos, etc.)  y deja claro que son juegos y no competencias, esto permitirá que se sientan cómodos y se puedan realizar las actividades."
+  },
+  {
+    name: "  Muestra interés por los deportes",
+    label: "  Muestra interés por los deportes",
+    alertMessage: " Enséñale que la actividad física debes ser tomada como un juego y no como una obligación o una competencia entre padres y niños."
+  },
+  {
+    name: "  No le gusta ser tratado como un niño",
+    label: "  No le gusta ser tratado como un niño",
+    alertMessage: " Acepta sus emociones sin juzgarlo, con esto reafirmas sus sentimientos y le demuestras que es importante para ti."
+  },
+  {
+    name: "  Tiene dudas e inseguridades",
+    label: "  Tiene dudas e inseguridades",
+    alertMessage: " Recuérdale a tu hijo que cuenta con las capacidades necesarias para la realización de las diferentes actividades. Demuestrele que confía en el."
+  },
+  {
+    name: "  Saca conclusiones basándose en lo que haya aprendido previamente",
+    label: "  Saca conclusiones basándose en lo que haya aprendido previamente",
+    alertMessage: " Deja que siga su intuición y sus inclinaciones naturales. Acompañalo y guialo en este proceso."
+  },
+  {
+    name: "  Tiene una relación positiva con sus compañeros",
+    label: "  Tiene una relación positiva con sus compañeros",
+    alertMessage: " No lo compares con otros niños."
+  },
+  {
+    name: "  Desarrollo de individualidad y personalidad",
+    label: "  Desarrollo de individualidad y personalidad",
+    alertMessage: " Aprovecha espacios dentro de tu casa o fuera de ella que permitan conectarse con la dimensión espiritual y fomentar el desarrollo de su personalidad."
   }
-];
+]
