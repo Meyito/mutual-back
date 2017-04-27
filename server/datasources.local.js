ac19ad3cfcd63beeb86a881d6d14cb27501e8666
Marcos J. Alvarez <garusis@gmail.com>
2017-04-26 21:25:09 -0500

    Add port as db datasource parameter

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -5,6 +5,7 @@ let path = require('path');
 let datasources = {
   'database': {
     'host': process.env.DATABASE_HOST || "pg",
+    'port': process.env.DATABASE_PORT || 5432,
     'database': process.env.DATABASE_NAME,
     'username': process.env.DATABASE_USERNAME,
     'password': process.env.DATABASE_PASSWORD,
